2a4189bfcabaef5ee140eb280bf0b5f55105cfe5
Stephen Valdinger <svalding@kent.edu>
2017-02-09 10:34:47 -0500

    Added Trust Relationships example file

---
+++ TrustRelationships.ps1
@@ -0,0 +1,11 @@
+﻿#Trust Relationship Failed errors. Those are fun aren't they? 
+
+
+Test-ComputerSecureChannel
+
+
+Test-ComputerSecureChannel -Repair -Credential DOMAIN\user
+
+
+
+Reset-ComputerMachinePassword 
