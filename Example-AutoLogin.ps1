b27a58e2b05c2077fb52a94584d5201d16c4bc10
steviecoaster <steviecoaster@users.noreply.github.com>
2017-02-09 09:12:33 -0500

    Added the rest of the function

--- Example-AutoLogin.ps1
+++ Example-AutoLogin.ps1
@@ -11,4 +11,28 @@ new-itemproperty -path "HKLM:\software\microsoft\Windows NT\currentversion\winlo
 #Better way, as a function!
 
 Function Add-Autologin{
-	
+	Param(
+    [cmdletBinding()]
+    [parameter(Mandatory=$true,Position=0)]
+    [string]$DefaultUser,
+    [parameter(Mandatory=$true,Position=1)]
+    [string]$Password,
+    [parameter(Mandatory=$true,Posiiton=2)]
+    [string]$Domain
+    )
+
+    Write-Verbose -Message "Setting AutoLogin bit to 1..."
+    New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -Value 1
+    
+    Write-Verbose  -Message "Setting default user..."
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\winlogon" -Name DefaultUserName -Value $DefaultUser -Type String
+    
+    Write-Verbose -Message "Setting default password..."
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -Value $Password -Type String
+    
+    Write-Verbose -Message "Setting default logon domain..."
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultDomain -Value $Domain -Type String
+
+    Write-Verbose -Message "All operations completed, exiting..."
+
+}
