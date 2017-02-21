9962e58805c61786a5a060c676df5ccacaec77d9
steviecoaster <steviecoaster@users.noreply.github.com>
2017-02-21 15:00:33 -0500

    More @#*%# to show.

---
+++ Helper.ps1
@@ -0,0 +1,11 @@
+﻿Function Some-Function{
+    Param(
+        [parameter(Mandatory=$true,Position=1)]
+        [string]$Value
+    )
+
+    $window = New-Object -ComObject wscript.shell
+    $show = $window.popup($Value)
+
+
+}
---
+++ Worker.ps1
@@ -0,0 +1,7 @@
+﻿ . "H:\Documents\Powershell Scripts\Feb 23 Talk\Helper.ps1" #Load in helper file
+
+Function Worker{
+   
+    Some-Function -Value "Hello from Helper Script!"
+
+}
