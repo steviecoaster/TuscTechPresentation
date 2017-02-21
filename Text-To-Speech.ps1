a3efd42192f3b764a69a149a98c8997f4341eeb3
steviecoaster <steviecoaster@users.noreply.github.com>
2017-02-21 15:02:15 -0500

    Text to speech exacmple

---
+++ Text-To-Speech.ps1
@@ -0,0 +1,6 @@
+﻿Add-Type -AssemblyName System.speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$speak.SelectVoice('Microsoft David Desktop')
+
+$speak.Speak('Good afternoon, Tusc Technology Association!')
+$speak.Dispose()
