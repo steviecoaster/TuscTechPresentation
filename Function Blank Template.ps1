19955cf5489fd19e926ab726313a0d7bb058cbb8
steviecoaster <steviecoaster@users.noreply.github.com>
2017-02-22 21:24:22 -0500

    Finalized a few files. Added presentation

---
+++ Function Blank Template.ps1
@@ -0,0 +1,23 @@
+﻿Function <#name#>{
+    #REGION Optional paramater declarations
+    Param(
+        [cmdletBinding()]
+        [Parameter(Mandatory=$true/$false,Position=0)]
+        [string][array][byte][int]$Variablename,
+        [Parameter()]
+        [string]$Secondvariable
+        #add as many or few as needed, comma separated after variable declaration.
+    )
+
+    <#
+
+
+
+    INSERT CODE CONTENT
+
+
+
+   #>
+
+
+   }#Close function
